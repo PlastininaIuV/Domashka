0547ed20cae983308c10fa249e55fb7fca46ecc7
Iuliia <j.plastinina@yandex.ru>
2023-04-22 13:03:18 +0500

    задача 4

---
+++ exampl003/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.Write("Введите число a:  ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите число b:  ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите число c:  ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b && a > c){
+    Console. WriteLine("a наибольшее число");
+}
+else if (b > a && b > c){
+    Console. WriteLine("b наибольшее число");
+}
+else{
+    Console. WriteLine("c наибольшее число");
+}
