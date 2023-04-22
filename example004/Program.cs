6f52ee293fc6462863fec63433993156e00c4e58
Iuliia <j.plastinina@yandex.ru>
2023-04-22 14:07:26 +0500

    задача 6

---
+++ example004/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+//4 -> да
+//-3 -> нет
+//7 -> нет
+
+Console.Write("Введите число a:  ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if((a % 2) == 0){
+    Console.WriteLine("введенное число a четное");
+}
+else{
+    Console.WriteLine("введенное число a нечетное");
+}
+
+
+
