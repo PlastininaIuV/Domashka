7d97096e22e321aff44afad22d316e29629304bb
Iuliia <j.plastinina@yandex.ru>
2023-05-01 12:23:22 +0500

    задача 10

--- example010/Program.cs
+++ example010/Program.cs
@@ -1,28 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
+﻿//Задача 10
 
-//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-//456 -> 5
-//782 -> 8
-//918 -> 1
+Console.Write("вводим число a:  ");
+int a = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("вводим число:  ");
-int num = Convert.ToInt32(Console.ReadLine());
-
-Console.Write($"{num} последняя цифра: ");
-
-if (num < 0) {
-    num *=-1;
-}
-if (num <= 99 && num >= -99) {
-    Console.Write ("третьей цифры нет");
+if (a > 99 && a < 1000) {
+    a1  =  a  %  100/10; 
+    Console.WriteLine ($" центральное число - { a1 }");
 }
 else{
-  
-    while (num >= 999){
-        num /= 10;
-    }
-    num = num % 10;
-    Console.WriteLine (num);
-}
-   
+    Console.WriteLine ( " Недопустимое значение! " );   
+}  
---
+++ example013/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//Задача 13
+
+Console.Write("вводим число:  ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"{num} последняя цифра: ");
+
+if (num < 0) {
+    num *=-1;
+}
+if (num <= 99 && num >= -99) {
+    Console.Write ("третьей цифры нет");
+}
+else{
+  
+    while (num >= 999){
+        num /= 10;
+    }
+    num = num % 10;
+    Console.WriteLine (num);
+}
+   
