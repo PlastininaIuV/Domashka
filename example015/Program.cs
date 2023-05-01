c6476a06ff82953db6be610627f0e825c4e05dfa
Iuliia <j.plastinina@yandex.ru>
2023-05-01 12:43:08 +0500

    задача 15

---
+++ example015/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 15
+
+Console.Write ( " Укажите номер недели: " );
+int  weakDay1  =  Convert.ToInt32 ( Console.ReadLine ( )) ;
+
+if ( слабый день1  >  0  &&  слабый день1  <  8 ){
+    if ( слабый день1  >  5 ){
+        Console.Write( " День недели выходной ");
+    }
+    else  Console.Write ( " День недели не выходной " );
+}  
+Console.Write ( " Недопустимое значение " );
+
+
