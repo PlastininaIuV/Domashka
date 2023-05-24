cdbb7dcde370bdf6fd5baa6775b78e5137f31544
Iuliia <j.plastinina@yandex.ru>
2023-05-24 23:13:51 +0500

    задача 23

---
+++ example023/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Введите число: ");
+double N = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+Console.Write($"{N} =");
+while(i <= N){
+    Console.Write($" {Math.Pow(i, 3)} ");
+    i++;
+}
