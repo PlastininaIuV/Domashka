0e9e16bf87896deb55998b51ae6087a64c808da5
Iuliia <j.plastinina@yandex.ru>
2023-06-04 12:42:54 +0500

    задача 52

---
+++ example052/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
